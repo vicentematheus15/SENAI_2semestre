066a3577ab07c611f3854caba73050e13774234e
Matheus Vicente <vicentematheus1507@gmail.com>
2025-08-27 21:38:27 -0300

    Refactor user registration function to improve readability and fix login logic

--- Programacao de aplicativos/exercicios/script.js
+++ Programacao de aplicativos/exercicios/script.js
@@ -1,26 +1,28 @@
 let _usuariosAutenticados = []
 
-    function cadastrar(){
-        const nome = document.getElementById("nomeCadastro").value
-        const senha = document.getElementById("senhaCadastro").value
+function cadastrar(){
+    const nome = document.getElementById("nomeCadastro").value
+    const senha = document.getElementById("senhaCadastro").value
 
-        let isValid = true
-        if(isValid){
-            const obj = {
-                nome: nome,
-                senha: senha
-            }
-            _usuariosAutenticados.push(obj)
-            alert("Cadastro concluído com sucesso")
-            localStorage.setItem("usuariosAutenticados", JSON.stringify(_usuariosAutenticados))
+    let isValid = true
+    if(isValid){
+        const obj = {
+            nome: nome,
+            senha: senha
         }
-        
-function login(){
-    for(index in _usuariosAutenticados){
-        let usuarioAutenticado = _usuariosAutenticados[index]
-        if(usuarioAutenticado.nome === nome){
-            alert
-        }
+    _usuariosAutenticados.push(obj)
+    alert("Cadastro concluído com sucesso")            
+    localStorage.setItem("usuariosAutenticados", JSON.stringify(_usuariosAutenticados))
     }
 }
+        
+function login(){
+    localStorage.getItem(JSON.parse(_usuariosAutenticados))
+
+    for(index in _usuariosAutenticados){
+        let usuarioAutenticado = _usuariosAutenticados[index]
+        if(usuarioAutenticado.nome === nome && usuarioAutenticado.senha === senha){
+            alert("Usuário autenticado")
+        }
+    }
 }
