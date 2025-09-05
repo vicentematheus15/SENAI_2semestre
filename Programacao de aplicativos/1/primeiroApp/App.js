26966160505d421e7e2b515acce6d0314b4e0913
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-04 22:10:20 -0300

    Add basic arithmetic operations to App component

--- Programacao de aplicativos/1/primeiroApp/App.js
+++ Programacao de aplicativos/1/primeiroApp/App.js
@@ -3,7 +3,29 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
+  const [primeiroNumero, setPrimeiroNumero] = useState(0);
+  const [segundoNumero, setSegundoNumero] = useState(0);
+  const [resultado, setResultado] = useState(0);
 
+  const soma = () =>{
+    const resultadoSoma = parseFloat(primeiroNumero) + parseFloat(segundoNumero);
+    setResultado(resultadoSoma)
+  }
+
+  const subtracao = () =>{
+    const resultadoSubtracao = parseFloat(primeiroNumero) - parseFloat(segundoNumero);
+    setResultado(resultadoSubtracao)
+  }
+
+  const multiplicacao = () =>{
+    const resultadoMultiplicacao = parseFloat(primeiroNumero) * parseFloat(segundoNumero);
+    setResultado(resultadoMultiplicacao)
+  }
+
+  const divisao = () =>{
+    const resultadoDivisao = parseFloat(primeiroNumero) / parseFloat(segundoNumero);
+    setResultado(resultadoDivisao)
+  }
 
   return (
     <View style={styles.container}>
