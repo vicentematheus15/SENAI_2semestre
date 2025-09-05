4ce2945e6b92fe3ba216648e8006aa325d3c4744
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-04 22:10:06 -0300

    Initialize Expo app structure with main components and configuration files

---
+++ Programacao de aplicativos/1/primeiroApp/App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+
+
+  return (
+    <View style={styles.container}>
+
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
