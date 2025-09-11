77d80208dabfa724878043494757ec7643b6d683
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-10 22:28:19 -0300

    Add Pessoa class with nome, rg, and enderecos fields

---
+++ Modelagem de Sistemas/Exemplo para representar diagrama de classe/Pessoa.java
@@ -0,0 +1,16 @@
+public class Pessoa{
+
+    private String nome;
+    private String rg;
+    private Endereco[] enderecos;
+
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+}
