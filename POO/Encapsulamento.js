79412a1177caf2a8fea2d937b9f54e23154bdd92
Matheus Vicente <vicentematheus1507@gmail.com>
2025-10-28 20:38:21 -0300

    fix: update ContaBancaria to use private class fields for saldo and add example usage

--- POO/Encapsulamento.js
+++ POO/Encapsulamento.js
@@ -1,13 +1,17 @@
 // encapsulamento é a tecnica de "esconder" detalhes internos de uma classe e controlar o acesso a ela
 
 class ContaBancaria {
-    $saldo = 0; //atributo privado é só colocar "$" na frente
+    #saldo = 0; //atributo privado é só colocar "#" na frente
 
     depositar(valor){
-        this.$saldo += valor;
+        if (valor > 0) this.#saldo += valor;
     }
 
     verSaldo(){
-        return this.$saldo;
+        return this.#saldo;
     }
 }
+
+const conta = new ContaBancaria();
+conta.depositar(500);
+console.log(conta.verSaldo());
