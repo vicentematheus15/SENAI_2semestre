0f45bcebe825ec6f8ddc4a54ee9a5d1555e5260a
Matheus Vicente <vicentematheus1507@gmail.com>
2025-10-28 19:54:58 -0300

    feat: implement Funcionario class extending Pessoa with methods for working and speaking

--- POO/heranca.js
+++ POO/heranca.js
@@ -1,8 +1,8 @@
 //herança permite que a classe (filho) reaproveite e extenda comportamentos de outra classe (pai)
 
-class  Funcionario extends Pessoa {
+class Pessoa {
     //atributos
-    constructor(nome, idade, cargo){    
+    constructor(nome, idade){    
         this.nome = nome;
         this.idade = idade;
     }
@@ -11,3 +11,20 @@ class  Funcionario extends Pessoa {
         console.log(`Olá, meu nome é ${this.nome}`)
     }
 }
+
+class  Funcionario extends Pessoa {
+    //atributos
+    constructor(nome, idade, cargo){    
+        super(nome, idade); //recebe da classe pai (pessoa)
+        this.cargo = cargo;
+    }
+    //métodos
+    trabalhar(){
+        console.log(`${this.nome} está trabalhando como ${this.cargo}`);
+    }
+    
+}
+
+const funcionario1 = new Funcionario("Jhon", 20, "Desenvolvedor");
+funcionario1.trabalhar();
+funcionario1.falar();
