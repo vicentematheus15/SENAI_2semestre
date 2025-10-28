ad9ed81ea470771b70e9341a8e456ed74a358771
Matheus Vicente <vicentematheus1507@gmail.com>
2025-10-28 19:40:57 -0300

    feat: implement Pessoa class with constructor and method for greeting

---
+++ POO/classe.js
@@ -0,0 +1,18 @@
+//é um modelo que define um objeto e as caracteristicas dele
+class Pessoa {
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    falar(){
+        console.log(`Olá, meu nome é ${this.nome}`)
+    }
+}
+
+//testando classe
+const pesssoa1 = new Pessoa("Natan", 25);
+pesssoa1.falar(); //chama o método "falar"
+
+const pessoa2 = new Pessoa("Maria", 30);
+pessoa2.falar();
